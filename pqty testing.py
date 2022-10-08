ef1a805438a1a0c4fd0a32dbfc0ceee1a229c1e6
devinpoindexter <devin.poindexter@gmail.com>
2022-10-08 13:21:16 -0700

    wifi

--- pqty testing.py
+++ pqty testing.py
@@ -1,11 +1,23 @@
 # import module
+from re import I
 from wifi import Cell, Scheme
  
 # scan available Wifi networks
 raw_networks = list(Cell.all('wlan0'))
-networks = []
-ssids = set()
+networks = dict()
 for network in raw_networks:
-    if network.ssid not in ssids:
-        networks.append(network)
-        ssids.add(network.ssid)
+    if network.ssid not in networks.keys():
+        networks[network.ssid] = network
+
+
+output = list(networks.keys())
+
+for i,v in enumerate(output):
+    print(f'{i}:{v}')
+
+selection = input()
+
+if output[selection]:
+    Print(f'Connecting to {output[selection]}')
+else:
+    print('Please make a valid selection')
